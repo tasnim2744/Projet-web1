7bfc9e075ba3c83f1aade4044fa2fe84de6a48ee
tasnim2744 <chehibitasnim1@gmail.com>
2025-12-15 21:20:31 +0100

    Delete TasnimCRUD directory

--- TasnimCRUD/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeController {
-    
-    public function index() {
-        // Render homepage view
-        include __DIR__ . '/../views/home.php';
-    }
-}
--- TasnimCRUD/events.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/controllers/EventController.php';
-$ev = new EventController();
-$ev->index();
